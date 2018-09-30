4ea48033d6e865c4f30fe5bf96c2c2cb32b6acfa
Andrea Tosatto <andrea@themostaza.com>
2018-09-30 13:02:37 +0200

    Changed repo url in setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup
 setup(
     name='tp-connected',
     packages=['tp_connected'],
-    version='0.0.1',
+    version='0.0.2',
     install_requires=['aiohttp>=3.0.1','attrs'],
     description='TP-Link LTE modem API',
     author='Andrea Tosatto',
-    url='https://github.com/amelchio/eternalegypt',
+    url='https://github.com/andtos90/tp-connected',
     license='MIT',
     keywords=['tp-link,lte,MR6400'],
     classifiers=[
